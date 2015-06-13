2371d76c444c2bb2f1ddc0f8b4d88720964c8008
Peter Fronc <peter@theprivateland.com>
2015-06-14 00:10:55 +0100

    Adding watcher class.

--- src/com/qubitproducts/compilejs/Watcher.java
+++ src/com/qubitproducts/compilejs/Watcher.java
@@ -34,89 +34,122 @@ import java.nio.file.WatchService;
 import java.nio.file.WatchEvent.Kind;
 import java.nio.file.attribute.BasicFileAttributes;
 
-
 /**
  *
  * @author Peter Fronc <peter.fronc@qubitdigital.com>
  */
 public class Watcher {
-    
-    public static void main(String[] args) 
-        throws IOException, InterruptedException{
-        new Watcher().watch();
+
+    public static void main(String[] args)
+        throws IOException, InterruptedException {
+        
+        Callback callback = new Callback(){
+            @Override
+            public void call(Object o) {
+                
+            }
+        };
+        
+        new Watcher().watch(
+            System.getProperty("user.home") + "/xxx",
+            callback);
     }
+
     
     
-    public void watch()
+    public void watch(String path, Callback callback)
         throws
         IOException,
         InterruptedException {
 
-        final Path myDir = Paths.get("xxxxxxxxxx");
+        final Path myDir = Paths.get(path);
+        
         try {
             final WatchService watcher = 
                 myDir.getFileSystem().newWatchService();
-            
+
             final Kind[] kinds = new WatchEvent.Kind[]{
+                StandardWatchEventKinds.OVERFLOW,
                 StandardWatchEventKinds.ENTRY_CREATE,
                 StandardWatchEventKinds.ENTRY_DELETE,
-                StandardWatchEventKinds.ENTRY_MODIFY,
-                StandardWatchEventKinds.OVERFLOW
+                StandardWatchEventKinds.ENTRY_MODIFY
             };
-            
-            myDir.register(watcher, kinds, SensitivityWatchEventModifier.HIGH);
-            
+
+            myDir.register(watcher, kinds, 
+                SensitivityWatchEventModifier.HIGH);
+
             registerTree(myDir, watcher, kinds);
-            
             System.out.println("Attached");
-            
-            while(true) {
-                // Obtaining watch keys
+
+            while (true) {
                 final WatchKey key = watcher.take();
-                //if (key == null) continue;
+                //if (key == null) continue; //poll case
                 // key value can be null if no event was triggered
                 for (WatchEvent<?> watchEvent : key.pollEvents()) {
                     final Kind<?> kind = watchEvent.kind();
                     // Overflow event
                     if (StandardWatchEventKinds.OVERFLOW == kind) {
                         continue; // loop
-                    } else if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
-                        Path tmp = ((Path)watchEvent.context());
-//                        final Path child = myDir.resolve(tmp);
-                        System.out.println("Created " + tmp);
-                        registerTree(myDir, watcher, kinds);
-                    } else if (kind == StandardWatchEventKinds.ENTRY_DELETE) {
-                        System.out.println("Deleted " + watchEvent.context());
-                    } else if (kind == StandardWatchEventKinds.ENTRY_MODIFY) {
-                        System.out.println("Modified " + watchEvent.context());
+                    } else {
+                        if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
+                            Path dir = (Path) key.watchable();
+                            dir = dir.resolve(((Path) watchEvent.context()));
+    //                        final Path child = myDir.resolve(tmp);
+                            System.out.println("Created " + dir.toAbsolutePath());
+                            registerTree(dir, watcher, kinds);
+                        } else if (kind == StandardWatchEventKinds.ENTRY_DELETE) {
+                            System.out.println("Deleted " + watchEvent.context());
+                        } else if (kind == StandardWatchEventKinds.ENTRY_MODIFY) {
+                            System.out.println("Modified " + watchEvent.context());
+                        }
+                        
+                        if (callback != null) {
+                            callback.call(watchEvent.context());
+                        }
                     }
                 }
-                
-                if(!key.reset()) {
-					break;
-				}
+
+                if (!key.reset()) {
+                    break;
+                }
             }
         } finally {
         }
-        
-    }
 
+    }
+    
+    int maxErrorMsgs = 99;
+    volatile int maxErrorCount = 0;
+    
     private void registerTree(
         Path myDir,
         final WatchService watcher,
-        final Kind[] kinds) 
+        final Kind[] kinds)
         throws IOException {
-        
+
         Files.walkFileTree(myDir, new SimpleFileVisitor<Path>() {
-                @Override
-                public FileVisitResult preVisitDirectory(
-                    Path dir,
-                    BasicFileAttributes attrs)
+            @Override
+            public FileVisitResult preVisitDirectory(
+                final Path dir,
+                BasicFileAttributes attrs)
+                throws IOException {
+                if (!Files.isHidden(dir)) {
                     
-                    throws IOException {
-                    dir.register(watcher, kinds, SensitivityWatchEventModifier.HIGH);
-                        return FileVisitResult.CONTINUE;
+                    try{
+                       
+                        dir.register(watcher, kinds, SensitivityWatchEventModifier.HIGH);
+                        System.out.println("Created " + dir.toAbsolutePath());
+                    } catch (Exception ex) {
+                        if (maxErrorCount++ < maxErrorMsgs)
+                        System.out.println("Cannot register watch at file " + 
+                            dir + "\n. Exception:" + ex.getMessage());
+                        if (maxErrorCount == maxErrorMsgs) {
+                            System.out.println("Stopping errors log...");
+                        }
+                    }
                 }
-            });
+                return FileVisitResult.CONTINUE;
+            }
+        });
     }
 }
