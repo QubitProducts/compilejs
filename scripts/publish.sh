8f118de3c8a406c6eb60b1cea34b4424d2c20e5f
Peter Fronc <peter.fronc@qubit.com>
2015-07-21 12:07:38 +0100

    Publishing current release.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,9 +8,11 @@ echo "Compiling."
 ant jar
 
 echo "Copying files."
+
 cp -v dist/*.jar current_release/
 cp -v dist/compilejs.jar ../opentag-libraries/
 cp -v dist/compilejs.jar ../opentag/nbproject/lib/compilejs.jar
 cp -v dist/compilejs.jar ../tagsdk/nbproject/lib/compilejs.jar
+cp -v dist/compilejs.jar ../tagsdk-build-tool/lib/compilejs.jar
 
 echo "Finished."
